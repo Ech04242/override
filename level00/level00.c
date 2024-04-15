38f7c8cbaf900be173808a1a60f019cfa0959358
Theo LASSARAT <tlassara@bess-f2r1s17.clusters.42paris.fr>
2024-04-15 15:46:47 +0200

    edit return value source

--- level00/level00.c
+++ level00/level00.c
@@ -1,14 +1,18 @@
 int main(void)
 {
+    int ret;
     int password;
 
     printf("Password: ");
     scanf("%d", &password);
-    if (password != 5276)
+    if (password != 5276){
         printf("\nInvalid Password!\n");
+        ret = 0;
+    }
     else {
         printf("\nAuthenticated!\n");
         system("/bin/sh");
+        ret = 1;
     }
-    return (password != 5276);
+    return (ret);
 }
